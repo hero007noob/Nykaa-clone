6f6057d4dcb9915eb32908e0889bcec69fbdade3
maharshdhasmana <dhasmana.maharsh76@outlook.com>
2023-01-05 05:01:00 +0530

    payment final

--- Payment/payment.js
+++ Payment/payment.js
@@ -1,3 +1,4 @@
+// document.getElementById('firsttab').();
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -12,3 +13,25 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+    
+  } 
+  document.getElementById("firsttab").click();
