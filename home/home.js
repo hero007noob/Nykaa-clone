dce56cbc377e0d79da635374986cda4816325221
rasikaG23 <rasikargujar23@gmail.com>
2023-01-05 18:43:25 +0530

    homepagechanges

--- home/home.js
+++ home/home.js
@@ -364,10 +364,90 @@ function setCarouselRegular(allData, parentDiv) {
 }
 
 
-var slide = document.getElementById("slider");
-        var btn1 = document.getElementById('btn1');
-        var btn2 = document.getElementById('btn2');
-        var btn3 = document.getElementById('btn3');
-        var btn4 = document.getElementById('btn4');
+//1stc carosal js 
+ 
+//start
+
+const carouselSlide = document.querySelector('.carousel-slide'); 
+const carouselImages = document.querySelectorAll('.carousel-slide img'); 
+//Buttons 
+const prevBtn = document.querySelector('#prevBtn'); 
+const nextBtn = document.querySelector('#nextBtn'); 
+//Counter 
+let counter = 1; 
+const size = carouselImages[0].clientWidth; 
+carouselSlide.style.transform = 'translateX(' + (-size *counter) + 'px)'; 
+
+//Button Listeners 
+nextBtn.addEventListener('click', () => { 
+    if (counter >= carouselImages.length - 1) return; 
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"; 
+    counter++; 
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+}); 
+
+prevBtn.addEventListener('click', () => { 
+    if (counter <= 0) return; 
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"; 
+    counter--; 
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+}); 
+
+carouselSlide.addEventListener('transitionend', () => { 
+    if (carouselImages [counter].id === 'lastClone') { 
+        carouselSlide.style.transition = "none"; 
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+    } 
+    if (carouselImages [counter].id === 'firstClone') { 
+        carouselSlide.style.transition = "none"; 
+        counter = carouselImages.length - counter; 
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+    } 
+});
+
+//end
+
+
+//3rd slider 
+//start
+let span = document.getElementsByTagName('i');
+	let product = document.getElementsByClassName('product')
+	let product_page = Math.ceil(product.length/5);
+	let l = 0;
+	let movePer = 20.34;
+	let maxMove = 175;
+	// mobile_view	
+	let mob_view = window.matchMedia("(max-width: 768px)");
+	if (mob_view.matches)
+	 {
+	 	movePer = 50.36;
+	 	maxMove = 504;
+	 }
+
+	let right_mover = ()=>{
+		l = l + movePer;
+		if (product == 1){l = 0; }
+		for(const i of product)
+		{
+			if (l > maxMove){l = l - movePer;}
+			i.style.left = '-' + l + '%';
+		}
+
+	}
+	let left_mover = ()=>{
+		l = l - movePer;
+		if (l<=0){l = 0;}
+		for(const i of product){
+			if (product_page>1){
+				i.style.left = '-' + l + '%';
+			}
+		}
+	}
+	span[1].onclick = ()=>{right_mover();}
+	span[0].onclick = ()=>{left_mover();}
+
+
+//end
 
 
