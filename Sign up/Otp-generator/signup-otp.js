00cb92b1fff71e9e0e64a79c208cd10d7abde075
Anand R P <106311407+AnandRP2030@users.noreply.github.com>
2023-01-07 21:45:57 +0530

    Update signup-otp.js

--- Sign up/Otp-generator/signup-otp.js
+++ Sign up/Otp-generator/signup-otp.js
@@ -63,7 +63,7 @@ window.onload = function () {
 
 function otpSuccessful() {
   let pop = document.getElementsByClassName("pop-up-alert")[0];
-  pop.classList.add("pop");``
+  pop.classList.add("pop");
   let message = pop.querySelector(".pop-up-msg");
   message.textContent = "Registration completed ";
   setTimeout(() => {
